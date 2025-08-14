b8a0fe9d3b5b96075df3b6e692b2594e7c397f33
abnersamuel2010 <tomaz.abner@escola.pr.gov.br>
2025-08-14 08:39:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-
 function exibirValor() {
     const select = document.getElementById("filme");
     const valorSelecionado = select.value;
-    alert("O valor selecionado é: " + valorSelecionado);
+    document.getElementById('categoria').textContent = 'Categoria: ' + valorSelecionado;
 }
+const terror = [
+    { id: "tr", nome: "Terrifier", preço: 25 },
+    { id: "hp", nome: "Hora do pesadelo", preço: 25 },
+    { id: "s13", nome: "Sexta-feira 13  ", preço: 25 },
+]
+
+const aventura = [
+    { id: "jj", nome: "Jumanji", preço: 25 },
+    { id: "vg", nome: "Vingadores", preço: 25 },
+    { id: "vf", nome: "Velores e Furiosos", preço: 25 },
+]
+
+const romance = [
+    { id: "tc", nome: "Titanic", preço: 25 },
+    { id: "cv", nome: "Como eu era antes de você", preço: 25 },
+    { id: "10o", nome: "10 coisas que eu odeio você", preço: 25 },
+]
+
+const comedia = [
+    { id: "mg", nome: "Um maluco no golf", preço: 25 },
+    { id: "dt", nome: "De repente trinta", preço: 25 },
+    { id: "gg", nome: "Gente grande", preço: 25 },
+]
+
+const drama = [
+    { id: "ce", nome: "Culpa das estrelas", preço: 25 },
+    { id: "st", nome: "Show de truman", preço: 25 },
+    { id: "it", nome: "Interstelar", preço: 25 },
+]
